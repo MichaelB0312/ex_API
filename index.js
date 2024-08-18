7bbe1500eefa67f1394d4156ce1a0dca5c71537e
michaelB0312 <mberko18@gmail.com>
2024-08-18 15:05:27 +0300

    options form

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
-// HINTS:
-// 1. Import express and axios
 import express from "express";
+import bodyParser from "body-parser";
 import axios from "axios";
 
-// 2. Create an express app and set the port number.
 const app = express();
 const port = 3000;
-// 3. Use the public folder for static files.
+
 app.use(express.static("public"));
-// 4. When the user goes to the home page it should render the index.ejs file.
-// app.get("/", (req, res) => {
-//     res.render("index.ejs");
-//   });
-// 5. Use axios to get a random secret and pass it to index.ejs to display the
-// secret and the username of the secret.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Step 1: Make sure that when a user visits the home page,
+//   it shows a random activity.You will need to check the format of the
+//   JSON data from response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
 
-    try {
-      const response = await axios.get("https://secrets-api.appbrewery.com/random");
-      const result = response.data;
-      //result = JSON.stringify(result);
-      //console.log(JSON.stringify(result));
-      res.render("index.ejs", { secret: result.secret,
-                                user: result.username
-       });
-    } catch (error) {
-      console.error("Failed to make request:", error.message);
-      res.render("index.ejs", {
-        error: error.message,
-      });
-    }
-  });
+app.post("/", async (req, res) => {
+  console.log(req.body);
+  const usr_type = req.body["type"];
+  const usr_participants = req.body["participants"];
+  // Step 2: Play around with the drop downs and see what gets logged.
+  // Use axios to make an API request to the /filter endpoint. Making
+  // sure you're passing both the type and participants queries.
+  try {
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${usr_type}&participants=${usr_participants}`);
+    const result = response.data;
+    // Render the index.ejs file with a single *random* activity that comes back
+    // from the API request.
+    console.log(result.length);
+    var idx = Math.floor(Math.random()*result.length);
+    res.render("index.ejs", { data: result[idx] });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    //console.error(error.response.status);
+    res.render("index.ejs", {
+      error: error.message,
+      error_stat: error.response.status
+    });
+  }
+  // Step 3: If you get a 404 error (resource not found) from the API request.
+  // Pass an error to the index.ejs to tell the user:
+  // "No activities that match your criteria."
+});
 
-// 6. Listen on your predefined port and start the server.
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
+  console.log(`Server running on port: ${port}`);
+});
